9a0269753de6d81d1873e02074c34600ffde36fd
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-01-28 04:06:37 +0500

    homePage Created

--- src/s.js
+++ src/s.js
@@ -14,7 +14,7 @@ function App() {
         <link rel="stylesheet" href="assets/theme/css/style.css" />
         <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'" />
         <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" /></noscript>
-        <link rel="preload" as="style" href={"assets/all/css/mbr-additional.css"} /><link rel="stylesheet" href={"assets/all/css/mbr-additional.css"} type="text/css" />
+        <link rel="preload" as="style" href={"assets/mobirise/css/mbr-additional.css"} /><link rel="stylesheet" href={"assets/mobirise/css/mbr-additional.css"} type="text/css" />
       </head>
       <body>
 
@@ -24,13 +24,13 @@ function App() {
             <div className="container">
               <div className="navbar-brand">
                 <span className="navbar-logo">
-                  <a href={"/"}>
-                    <img src={"assets/images/mbr-121x81.jpg"} alt="menu" style={{
+                  <a href="https://mobiri.se">
+                    <img src={"assets/images/mbr-121x81.jpg"} alt="Mobirise Website Builder" style={{
                       height: '3.8rem',
                     }} />
                   </a>
                 </span>
-                <span className="navbar-caption-wrap"><a className="navbar-caption text-black display-7" href={"/"}>Temp-Details</a></span>
+                <span className="navbar-caption-wrap"><a className="navbar-caption text-black display-7" href="https://mobiri.se">Temp-Details</a></span>
               </div>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <div className="hamburger">
@@ -41,10 +41,7 @@ function App() {
                 </div>
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
-                  <li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>Home<br />
-                  </a></li><li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>Generate Details</a>
-                  </li><li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>Contact Us</a></li><li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>About</a></li></ul>
+                <ul className="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li className="nav-item"><a className="nav-link link text-black display-4" href="https://mobirise.com">Home<br /></a></li><li className="nav-item"><a className="nav-link link text-black display-4" href="https://mobirise.com">Generate Details</a></li><li className="nav-item"><a className="nav-link link text-black display-4" href="https://mobirise.com">Contact Us</a></li><li className="nav-item"><a className="nav-link link text-black display-4" href="https://mobirise.com">About</a></li></ul>
 
 
               </div>
@@ -68,7 +65,7 @@ function App() {
                 <h1 className="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>Generate Temp-Details</strong></h1>
 
                 <p className="mbr-text mbr-fonts-style display-7">Here You Can Easily <strong><em>Generate Temp-Details</em></strong> Which Will Be Use Anywhere as<em> You want to use</em>.</p>
-                <div className="mbr-section-btn mt-3"><a className="btn btn-success display-4" href={"/"}>Generate</a></div>
+                <div className="mbr-section-btn mt-3"><a className="btn btn-success display-4" href="https://mobirise.com">Generate</a></div>
               </div>
             </div>
           </div>
@@ -96,7 +93,7 @@ function App() {
             <div className="row justify-content-center">
               <div className="col-12 col-lg-9">
                 <div className="image-wrapper">
-                  <a href="https://www.mei.edu/sites/default/files/styles/featured_image_article/public/2021-01/Sigheh%20Certificate.jpg?itok=hvYpSbY9"><img src="assets/images/mbr-1646x1097.jpg" alt="WebsiteBuild" /></a>
+                  <a href="https://www.mei.edu/sites/default/files/styles/featured_image_article/public/2021-01/Sigheh%20Certificate.jpg?itok=hvYpSbY9"><img src="assets/images/mbr-1646x1097.jpg" alt="Mobirise Website Builder" /></a>
                   <p className="mbr-description mbr-fonts-style mt-2 align-center display-4">
                     Image Description</p>
                 </div>
@@ -111,9 +108,8 @@ function App() {
             <div className="row justify-content-center">
               <div className="title col-12 col-md-9">
                 <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>Add new blocks</strong></h3>
-                <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">
-                  Temp-Details can generate you random details which you will use Anywhere
-                </h4>
+                <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Mobirise offers several themes to work with. Each theme includes a set of various blocks: headers, content blocks, galleries and sliders, footers, etc. To add needed blocks to your page, click on <em>the red plus button </em>at the bottom right corner. You can select a needed block type. To add a block, click on it in the list or drag it to your page.</h4>
+
               </div>
             </div>
           </div>
@@ -139,7 +135,7 @@ function App() {
               <div className="title col-12 col-md-9">
                 <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>Publish your project
                 </strong></h3>
-                <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Click on the <strong>Publish </strong>button at the top right corner and select a needed publishing option. Use the publish option to publish your test site on our servers. You can publish your site locally if you don't want to publish it online yet.</h4>
+                <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Click on the <strong>Publish </strong>button at the top right corner and select a needed publishing option. Use the Mobirise publish option to publish your test site on our servers. You can publish your site locally if you don't want to publish it online yet.</h4>
 
               </div>
             </div>
@@ -161,10 +157,7 @@ function App() {
 
 
 
-                  <li className="foot-menu-item mbr-fonts-style display-7"><a href={"/"} className="text-success">Help Center</a ></li>
-                  <li className="foot-menu-item mbr-fonts-style display-7"><a href={"/"} className="text-success">Forums</a></li><li className="foot-menu-item mbr-fonts-style display-7">
-                    <a href={"/"} className="text-success">#</a>
-                  </li></ul>
+                  <li className="foot-menu-item mbr-fonts-style display-7"><a href="https://mobirise.com/help/" className="text-success">Help Center</a></li><li className="foot-menu-item mbr-fonts-style display-7"><a href="http://forums.mobirise.com/" className="text-success">Mobirise Forums</a></li><li className="foot-menu-item mbr-fonts-style display-7"><a href="https://mobirise.com/" className="text-success">Mobirise.com</a></li></ul>
               </div>
               <div className="row social-row">
                 <div className="social-list align-right pb-2">
@@ -175,15 +168,15 @@ function App() {
 
 
                   <div className="soc-item">
-                    <a href={"/"}>
+                    <a href="https://twitter.com/mobirise">
                       <span className="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
                     </a>
                   </div><div className="soc-item">
-                    <a href={"/"}>
+                    <a href="https://www.facebook.com/pages/Mobirise/1616226671953247">
                       <span className="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
                     </a>
                   </div><div className="soc-item">
-                    <a href={"/"}>
+                    <a href="https://www.youtube.com/c/mobirise">
                       <span className="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
                     </a>
                   </div><div className="soc-item">
@@ -200,7 +193,32 @@ function App() {
             </div>
           </div>
         </section>
-
+        <section className="display-7" style={{
+          padding: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          alignContent: 'center',
+          display: 'flex',
+          position: 'relative',
+          height: '4rem',
+        }}>
+          <a href={"https://mobiri.se/2926682"} style={{
+            flex: '1 1',
+            height: '4rem',
+            position: 'absolute',
+            width: '100 %',
+            zIndex: '1',
+          }}>
+            <img alt="" style={{
+              height: '4rem',
+            }} src={"data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="} />
+          </a><p style={{
+            margin: 0,
+            textAlign: 'center'
+          }} className="display-7">Created with &#8204;</p><a style={{
+            zIndex: '1',
+          }} href={'/'}>Landing Page Creator</a></section>
         <script src={"assets/bootstrap/js/bootstrap.bundle.min.js"}></script>  <script src={"assets/parallax/jarallax.js"}></script>
         <script src={"assets/smoothscroll/smooth-scroll.js"}></script>  <script src={"assets/ytplayer/index.js"}></script>
         <script src={"assets/dropdown/js/navbar-dropdown.js"} ></script>  <script src={"assets/theme/js/script.js"}></script>
