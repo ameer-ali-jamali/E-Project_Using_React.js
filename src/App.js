82e6ae910efa94323cd584aded06af5483507db5
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-01-28 13:36:18 +0500

    home pagemfull responsive

--- src/App.js
+++ src/App.js
@@ -1,80 +1,156 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
       <section className="menu cid-s48OLK6784" alt="menu" id="menu1-h">
-
         <nav className="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
           <div className="container">
             <div className="navbar-brand">
-              <span className="navbar-logo">
+              {/* <span className="navbar-logo">
                 <a href={"/"}>
-                  <img src={"assets/images/mbr-121x81.jpg"} alt="menu" style={{
-                    height: '1.8rem',
-                  }} />
+                  <img
+                    src={"assets/images/mbr-121x81.jpg"}
+                    alt="menu"
+                    className="nav-img"
+                  />
+                </a>
+              </span> */}
+              <span className="navbar-caption-wrap">
+                <a className="navbar-caption text-black display-7" href={"/"}>
+                  Temp-Details
                 </a>
               </span>
-              <span className="navbar-caption-wrap"><a className="navbar-caption text-black display-7" href={"/"}>Temp-Details</a></span>
             </div>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
+            <button
+              className="navbar-toggler navbar-"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              style={{
+                boxSizing: "border-box",
+                border: "1px solid dodgerblue",
+                fontSize: "xx-large"
+              }}
+            >
+              <span
+                class="fas fa-bars"
+                style={{
+                  color: "darkorange",
+                  fontSize: "xx-large"
+                }}
+              ></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarContent">
-              <ul className="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
-                <li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>Home<br />
-                </a></li><li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>Generate Details</a>
-                </li><li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>Contact Us</a></li><li className="nav-item"><a className="nav-link link text-black display-4" href={"/"}>About</a></li></ul>
+              <ul
+                className="navbar-nav nav-dropdown nav-right"
+                data-app-modern-menu="true"
+              >
+                <li className="nav-item">
+                  <a className="nav-link link text-black display-4" href={"/"}>
+                    Home
+                    <br />
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link link text-black display-4" href={"/"}>
+                    Generate Details
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link link text-black display-4" href={"/"}>
+                    Contact Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link link text-black display-4" href={"/"}>
+                    About
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </nav>
+      </section>
 
-      </section >
-
-      <div className="header1 cid-s48MCQYojq mbr-fullscreen mbr-parallax-background" id="header1-f">
-        <div className="mbr-overlay" style={{
-          opacity: 0.8,
-          backgroundColor: 'white',
-        }}></div>
+      <div
+        className="header1 cid-s48MCQYojq mbr-fullscreen mbr-parallax-background"
+        id="header1-f"
+        style={{
+          color: "darkblue",
+          marginTop: -40
+        }}
+      >
+        <div
+          className="mbr-overlay"
+          style={{
+            opacity: 0.8,
+            backgroundColor: "white"
+          }}
+        ></div>
 
         <div className="align-center container">
           <div className="row justify-content-center">
             <div className="col-12 col-lg-9">
-              <h1 className="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>Generate Temp-Details</strong></h1>
+              <h1 className="mbr-section-title mbr-fonts-style mb-3 display-1">
+                <strong>Generate Temp-Details</strong>
+              </h1>
 
-              <p className="mbr-text mbr-fonts-style display-7">Here You Can Easily <strong><em>Generate Temp-Details</em></strong> Which Will Be Use Anywhere as<em> You want to use</em>.</p>
-              <div className="mbr-section-btn mt-3"><a className="btn btn-success display-4" href={"/"}>Generate</a></div>
+              <p className="mbr-text mbr-fonts-style display-7">
+                Here You Can Easily{" "}
+                <strong>
+                  <em>Generate Temp-Details</em>
+                </strong>{" "}
+                Which Will Be Use Anywhere as<em> You want to use</em>.
+              </p>
+              <div className="mbr-section-btn mt-3">
+                <a className="btn btn-success display-4" href={"/"}>
+                  Generate
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
       <div className="content1 cid-s48udlf8KU" id="content1-8">
-
         <div className="container">
           <div className="row justify-content-center">
             <div className="title col-12 col-md-9">
-              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>Work with Content</strong></h3>
-              <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Click on the image to replace it. To change parameters of a block, click on <em>the blue gear icon</em>. You can set a background type and its content. Click on the Buttons parameter to enable buttons for this block. Click in the text to change your text style or color. Select text to change its formatting or add links.</h4>
-
+              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2">
+                <strong>Work with Content</strong>
+              </h3>
+              <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">
+                Click on the image to replace it. To change parameters of a
+                block, click on <em>the blue gear icon</em>. You can set a
+                background type and its content. Click on the Buttons parameter
+                to enable buttons for this block. Click in the text to change
+                your text style or color. Select text to change its formatting
+                or add links.
+              </h4>
             </div>
           </div>
         </div>
       </div>
 
       <div className="image3 cid-s48upRUlSD" id="image3-9">
-
-
-
-
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-12 col-lg-9">
               <div className="image-wrapper">
-                <a href="https://www.mei.edu/sites/default/files/styles/featured_image_article/public/2021-01/Sigheh%20Certificate.jpg?itok=hvYpSbY9"><img src="assets/images/mbr-1646x1097.jpg" alt="WebsiteBuild" /></a>
+                <a href="https://www.mei.edu/sites/default/files/styles/featured_image_article/public/2021-01/Sigheh%20Certificate.jpg?itok=hvYpSbY9">
+                  <img
+                    src="assets/images/mbr-1646x1097.jpg"
+                    alt="WebsiteBuild"
+                  />
+                </a>
                 <p className="mbr-description mbr-fonts-style mt-2 align-center display-4">
-                  Image Description</p>
+                  Image Description
+                </p>
               </div>
             </div>
           </div>
@@ -82,13 +158,15 @@ function App() {
       </div>
 
       <div className="content1 cid-s48vaXqeL6" id="content1-b">
-
         <div className="container">
           <div className="row justify-content-center">
             <div className="title col-12 col-md-9">
-              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>Add new blocks</strong></h3>
+              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2">
+                <strong>Add new blocks</strong>
+              </h3>
               <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">
-                Temp-Details can generate you random details which you will use Anywhere
+                Temp-Details can generate you random details which you will use
+                Anywhere
               </h4>
             </div>
           </div>
@@ -96,77 +174,88 @@ function App() {
       </div>
 
       <div className="content1 cid-s48vnjULo4" id="content1-c">
-
         <div className="container">
           <div className="row justify-content-center">
             <div className="title col-12 col-md-9">
-              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>Create new pages</strong></h3>
-              <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">You can have multiple pages in your project. Find the menu icon at the top left corner. Move your cursor over it to open the main menu. Click on the <strong>Pages </strong>tab to manage your pages. Don't forget to set your page title and description. This info is used by browsers and search engines. The home page of your project cannot be removed.</h4>
-
+              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2">
+                <strong>Create new pages</strong>
+              </h3>
+              <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">
+                You can have multiple pages in your project. Find the menu icon
+                at the top left corner. Move your cursor over it to open the
+                main menu. Click on the <strong>Pages </strong>tab to manage
+                your pages. Don't forget to set your page title and description.
+                This info is used by browsers and search engines. The home page
+                of your project cannot be removed.
+              </h4>
             </div>
           </div>
         </div>
       </div>
 
       <div className="content1 cid-s48vrXhP0J" id="content1-d">
-
         <div className="container">
           <div className="row justify-content-center">
             <div className="title col-12 col-md-9">
-              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>Publish your project
-              </strong></h3>
-              <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">Click on the <strong>Publish </strong>button at the top right corner and select a needed publishing option. Use the publish option to publish your test site on our servers. You can publish your site locally if you don't want to publish it online yet.</h4>
-
+              <h3 className="mbr-section-title mbr-fonts-style align-center mb-4 display-2">
+                <strong>Publish your project</strong>
+              </h3>
+              <h4 className="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">
+                Click on the <strong>Publish </strong>button at the top right
+                corner and select a needed publishing option. Use the publish
+                option to publish your test site on our servers. You can publish
+                your site locally if you don't want to publish it online yet.
+              </h4>
             </div>
           </div>
         </div>
       </div>
 
       <section className="footer3 cid-s48P1Icc8J" id="footer3-i">
-
-
-
-
-
         <div className="container">
           <div className="media-container-row align-center mbr-white">
             <div className="row row-links">
               <ul className="foot-menu">
-
-
-
-
-
-                <li className="foot-menu-item mbr-fonts-style display-7"><a href={"/"} className="text-success">Help Center</a ></li>
-                <li className="foot-menu-item mbr-fonts-style display-7"><a href={"/"} className="text-success">Forums</a></li><li className="foot-menu-item mbr-fonts-style display-7">
-                  <a href={"/"} className="text-success">#</a>
-                </li></ul>
+                <li className="foot-menu-item mbr-fonts-style display-7">
+                  <a href={"/"} className="text-success">
+                    Help Center
+                  </a>
+                </li>
+                <li className="foot-menu-item mbr-fonts-style display-7">
+                  <a href={"/"} className="text-success">
+                    Forums
+                  </a>
+                </li>
+                <li className="foot-menu-item mbr-fonts-style display-7">
+                  <a href={"/"} className="text-success">
+                    #
+                  </a>
+                </li>
+              </ul>
             </div>
             <div className="row social-row">
               <div className="social-list align-right pb-2">
-
-
-
-
-
-
                 <div className="soc-item">
                   <a href={"/"}>
                     <span className="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
                   </a>
-                </div><div className="soc-item">
+                </div>
+                <div className="soc-item">
                   <a href={"/"}>
                     <span className="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
                   </a>
-                </div><div className="soc-item">
+                </div>
+                <div className="soc-item">
                   <a href={"/"}>
                     <span className="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
                   </a>
-                </div><div className="soc-item">
+                </div>
+                <div className="soc-item">
                   <a href={"/"}>
                     <span className="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
                   </a>
-                </div></div>
+                </div>
+              </div>
             </div>
             <div className="row row-copirayt">
               <p className="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
@@ -181,9 +270,8 @@ function App() {
       <script src={"assets/parallax/jarallax.js"}></script>
       <script src={"assets/smoothscroll/smooth-scroll.js"}></script>
       <script src={"assets/ytplayer/index.js"}></script>
-      <script src={"assets/dropdown/js/navbar-dropdown.js"} ></script>
+      <script src={"assets/dropdown/js/navbar-dropdown.js"}></script>
       <script src={"assets/theme/js/script.js"}></script>
-
     </>
   );
 }
