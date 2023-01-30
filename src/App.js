0aa0461856ca8cca34b78e3d908e7e7f6bf5f668
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-01-30 11:27:43 +0500

    componenets and props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+// import header from "./Components/header";
+// import footer from "./Components/footer";
+// import content from "./Components/content";
 import Welcome from "./Components/welcome";
-
+// function App() {
+//   return (
+//     <>
+//       <h1>hello</h1>
+//     </>
+//   );
+// }
 function App() {
   return (
     <div>
