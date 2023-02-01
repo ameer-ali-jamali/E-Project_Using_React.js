8b93302b2a5e43ebe3d9abf76e58ee7be84f2e09
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-02 02:25:11 +0500

    hooks

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import React from "react";
 export default function Header(props) {
+  const [count, setCount] = useState(222);
   return (
     <section className="menu cid-s48OLK6784" alt="menu" id="menu1-h">
       <nav className="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
@@ -9,6 +11,7 @@ export default function Header(props) {
             <span className="navbar-caption-wrap">
               <a className="navbar-caption text-black display-7" href={"/"}>
                 {props.title}
+                {count}
               </a>
             </span>
           </div>
