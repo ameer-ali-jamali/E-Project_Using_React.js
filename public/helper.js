a415e7995d70d176bb5831906223edd95902da49
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-03 14:50:08 +0500

    now table hidden by default

--- public/helper.js
+++ public/helper.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $("#generateNameBtn").click(function () {
     $("#clearBtn").show();
+    $("#showTable").show();
   });
   $("#clearBtn").click(function () {
     $("#clearBtn").hide();
