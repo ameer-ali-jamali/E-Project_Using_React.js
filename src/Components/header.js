2c5e8aa0530f848a555c6ac48058d0d7e696903d
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-05 16:34:15 +0500

    .

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 export default function Header(props) {
